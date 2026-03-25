d15640785ff2d6c646e0f9e0cf78bc570104c8fc
Shahid Niyash A <shahidniyash@gmail.com>
2026-03-25 09:53:27 +0530

    Update Leetcode1523.java

---
+++ Day2/Leetcode1523.java
@@ -0,0 +1,10 @@
+class Leetcode1523 {
+    public int countOdds(int low, int high) {
+        int num=(high-low)+1;
+        if(low%2==1 && high%2==1){
+            return (num/2)+1;
+        }
+        else{return num/2;}
+        
+    }
+}
