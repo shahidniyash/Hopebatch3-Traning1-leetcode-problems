c16db554238293a4a5c6b303c50ea56ccb1887f4
Shahid Niyash A <shahidniyash@gmail.com>
2026-03-25 19:50:27 +0530

    Update Leetcode724.java

--- Day5 Task/Leetcode724.java
+++ Day5 Task/Leetcode724.java
@@ -1,1 +1,20 @@
+class Leetcode724 {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            int rightSum = totalSum - leftSum - nums[i];
 
+            if(leftSum == rightSum){
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
