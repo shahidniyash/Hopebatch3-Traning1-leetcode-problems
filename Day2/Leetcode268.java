425c5ea1cc9f4fd2347b3b7c67e78de48a36f75a
Shahid Niyash A <shahidniyash@gmail.com>
2026-03-25 18:27:50 +0530

    Update Leetcode268.java

--- Day2/Leetcode268.java
+++ Day2/Leetcode268.java
@@ -1,1 +1,21 @@
-
+class Leetcode268 {
+    public int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int current = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[current]){
+                int temp = nums[i];
+                nums[i] = nums[current];
+                nums[current] = temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j){
+                return j;
+            }
+        }
+        return nums.length;
+    }
+}
