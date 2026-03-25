e6a36d163fe8a45cac103964efc7bb2661e3546f
Shahid Niyash A <shahidniyash@gmail.com>
2026-03-25 19:07:36 +0530

    Update Leetcode283.java

--- Day5/Leetcode283.java
+++ Day5/Leetcode283.java
@@ -1,1 +1,15 @@
-
+class Leetcode283 {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        while(k < nums.length){
+            nums[k] = 0;
+            k++;
+        }
+    }
+}
