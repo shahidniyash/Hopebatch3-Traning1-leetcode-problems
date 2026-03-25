1af4b1136eb2523bc9c627cbeaf045ccdbf9542e
Shahid Niyash A <shahidniyash@gmail.com>
2026-03-25 09:52:38 +0530

    Update Leetcode136.java

---
+++ Day2/Leetcode136.java
@@ -0,0 +1,11 @@
+class Leetcode136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        
+        for(int num : nums){
+            result ^= num;
+        }
+        
+        return result;
+    }
+}
